e81478ac714c1daf3ad9df9c83b6174365a6e085
Jason Pierna <siilver777@me.com>
2017-02-08 22:55:49 +0100

    Adds comments

--- SwifTools/SwifTools/STConstraints.swift
+++ SwifTools/SwifTools/STConstraints.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 public class STConstraints {
+    /**
+     * Enumerates the possible side of a constraint.
+     */
     public enum Sides {
         case left, right, top, bottom, leading, trailing
     }
     
+    /**
+     * Enumerates both size axis.
+     */
     public enum Sizes {
         case width, height
     }
     
+    /**
+     Creates a size constraint on the selected view.
+     - parameter view: The view to constraint
+     - parameter sizes: The desired axis
+     - parameter value: The size value
+    */
     public static func addSizeConstraints(view: UIView, sizes: [STConstraints.Sizes], value: CGFloat) {
         view.translatesAutoresizingMaskIntoConstraints = false
         
@@ -31,6 +43,13 @@ public class STConstraints {
         }
     }
     
+    /**
+     Creates a constraint between a view and one of its parent.
+     - parameter parent: The view's parent
+     - parameter child: The view ton constraint
+     - parameter sides: The side to constraint
+     - parameter constant: The distance between both views.
+     */
     public static func addConstraints(parent firstView: UIView, child secondView: UIView, sides: [STConstraints.Sides], constant: CGFloat) {
         firstView.translatesAutoresizingMaskIntoConstraints = false
         secondView.translatesAutoresizingMaskIntoConstraints = false
@@ -53,14 +72,33 @@ public class STConstraints {
         }
     }
     
+    /**
+     Creates a constraint between a view and one of its parent. The constant is 0 by default.
+     - parameter parent: The view's parent
+     - parameter child: The view to constraint
+     - parameter sides: The side to constraint
+     */
     public static func addConstraints(parent firstView: UIView, child secondView: UIView, sides: [STConstraints.Sides]) {
         addConstraints(parent: firstView, child: secondView, sides: sides, constant: 0)
     }
 
+    /**
+     Creates a constraint between two child views. The constant is 0 by default.
+     - parameter parent: The first view.
+     - parameter child: The second view.
+     - parameter sides: The side to constraint (relative to the first view)
+     */
     public static func addConstraints(between firstView: UIView, and secondView: UIView, sides: [STConstraints.Sides]) {
         addConstraints(between: firstView, and: secondView, sides: sides, constant: 0)
     }
     
+    /**
+     Creates a constraint between two child views.
+     - parameter parent: The first view.
+     - parameter child: The second view.
+     - parameter sides: The side to constraint (relative to the first view)
+     - parameter constant: The distance between both views.
+     */
     public static func addConstraints(between firstView: UIView, and secondView: UIView, sides: [STConstraints.Sides], constant: CGFloat) {
         firstView.translatesAutoresizingMaskIntoConstraints = false
         secondView.translatesAutoresizingMaskIntoConstraints = false
