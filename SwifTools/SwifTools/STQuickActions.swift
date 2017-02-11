5548e58a387a4427f7a8b17595a5ecefd764da93
Jason Pierna <siilver777@me.com>
2017-02-11 19:05:09 +0100

    Quick Actions class finished

--- SwifTools/SwifTools/STQuickActions.swift
+++ SwifTools/SwifTools/STQuickActions.swift
@@ -9,15 +9,32 @@
 import Foundation
 
 public class STQuickActions {
-    static let shared = STQuickActions()
+    /**
+     Singleton.
+    */
+    public static let shared = STQuickActions()
     
+    private init() {}
+    
+    /**
+     Enumerates every quick action position (1 to 4)
+    */
     public enum Action: String {
         case first, second, third, fourth
     }
     
     var items = [UIApplicationShortcutItem]()
+    var handlers = [Action: () -> Bool]()
     
-    public func add(title: String, subtitle: String? = nil, icon: UIApplicationShortcutIconType? = nil, userInfo: [AnyHashable : Any]? = nil) {
+    /**
+     Adds a new Home Screen Quick Action to the application.
+     - parameter title: The Quick Action's title
+     - parameter subtitle: The Quick Action's subtitletitle
+     - parameter icon: The Quick Action's icon
+     - parameter userInfo: The Quick Action's userInfo
+     - parameter handler: Block of code triggered by the quick action.
+     */
+    public func add(title: String, subtitle: String? = nil, icon: UIApplicationShortcutIconType? = nil, userInfo: [AnyHashable : Any]? = nil, handler: (() -> Bool)? = nil) {
         
         var image: UIApplicationShortcutIcon?
         if let icon = icon {
@@ -32,12 +49,20 @@ public class STQuickActions {
             items.append(shortcut)
             
             UIApplication.shared.shortcutItems = items
+            
+            if let handler = handler {
+                handlers[type] = handler
+            }
         }
         else {
             NSLog("SwifTools: Maximum quick actions reached")
         }
     }
     
+    /**
+     Removes a Home Screen Quick Action.
+     - parameter type: The type of the quick action to remove.
+     */
     public func remove(_ type: Action) {
         let index = items.index(where: { shortcut in
             return shortcut.type == type.rawValue
@@ -45,13 +70,52 @@ public class STQuickActions {
         
         if let index = index {
             items.remove(at: index)
+            
+            let handlerIndex = handlers.index(where: { (key, _) in
+                return key == type
+            })
+            
+            if let handlerIndex = handlerIndex {
+                handlers.remove(at: handlerIndex)
+            }
+            
             UIApplication.shared.shortcutItems = items
         }
     }
     
+    /**
+     Removes all Home Screen Quick Actions.
+     */
     public func removeAll() {
         items.removeAll()
+        handlers.removeAll()
         UIApplication.shared.shortcutItems = nil
+    }
+
+    /**
+     Handles the Quick Action when triggered. This method should be called inside AppDelegate's performAction method.
+     - parameter shortcutItem: the shortcut item triggered.
+     - parameter completionHandler: pass true if the quick action is handled.
+     */
+    public func performAction(shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        if let type = STQuickActions.Action(rawValue: shortcutItem.type) {
+            let manager = STQuickActions.shared
+            completionHandler(manager.handle(type))
+        }
+        else {
+            completionHandler(false)
+        }
+    }
+    
+    /**
+     Returns the handler to a specific quick action.
+     - parameter type: The type of the wanted quick action.
+     */
+    public func handle(_ type: Action) -> Bool {
+        if let handler = handlers[type] {
+            return handler()
+        }
+        return false
     }
     
     private func shortcutType(count: Int) -> Action {
@@ -68,5 +132,7 @@ public class STQuickActions {
             return .first
         }
     }
+    
+    
     
 }
